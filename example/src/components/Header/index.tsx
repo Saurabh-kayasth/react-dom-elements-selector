ee8f2369d929bc16353140e24772fd9b1bb04c66
Saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2023-07-19 13:48:31 +0530

    update package name

--- example/src/components/Header/index.tsx
+++ example/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.css'
 function Header() {
   return (
     <div className={styles.header__title}>
-      React&nbsp; <span className={styles.dom__text}>Elements</span>
+      React DOM&nbsp; <span className={styles.dom__text}>Elements</span>
       &nbsp;Selector
     </div>
   )
